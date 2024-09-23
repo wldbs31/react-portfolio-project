0005085a1e607788183b37d2e390eb76e7d98a73
wldbs31 <jiyunchoi31@gmail.com>
2024-09-23 13:18:13 -0400

    fetch genrelist

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Grid, GridItem, Show, Text } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
+import GenreList from "./components/GenreList";
 
 function App() {
   return (
@@ -15,7 +16,9 @@ function App() {
         <NavBar />
       </GridItem>{" "}
       <Show above="lg">
-        <GridItem area="aside">Nav</GridItem>{" "}
+        <GridItem area="aside">
+          <GenreList />
+        </GridItem>
       </Show>
       <GridItem area="main">
         <GameGrid />
--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Game } from "../hooks/useGames";
 import { Card, CardBody, Image, Heading, HStack } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
@@ -11,7 +10,7 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card width="300px" borderRadius={10} overflow="hidden">
+    <Card>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
