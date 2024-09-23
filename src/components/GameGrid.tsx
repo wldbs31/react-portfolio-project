d010267b4dd0675f7c9f788ff0a69f2ae88b0df5
wldbs31 <jiyunchoi31@gmail.com>
2024-09-23 16:13:58 -0400

    display genres and layout issue fixed

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,7 +1,4 @@
-import React, { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-
-import { list, SimpleGrid, Text } from "@chakra-ui/react";
+import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
@@ -17,7 +14,7 @@ const GameGrid = () => {
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
         padding="10px"
-        spacing={10}
+        spacing={3}
       >
         {isLoading &&
           skeletons.map((skeleton) => (
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,15 +1,25 @@
-import useData from "../hooks/useData";
+import { Text, Image, HStack, List, ListItem } from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { data } = useGenres();
 
   return (
-    <ul>
+    <List>
       {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <ListItem key={genre.id} paddingY="5px">
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            />
+            <Text fontSize="lg">{genre.name}</Text>
+          </HStack>{" "}
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
