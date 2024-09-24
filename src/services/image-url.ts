b2223596c352fdffc5f6a0cf515bea5f414c8d55
wldbs31 <jiyunchoi31@gmail.com>
2024-09-23 21:01:18 -0400

    sort the games

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,5 @@
 const getCroppedImageUrl = (url: string) => {
-  const target = "meida/";
+  if (!url) return "";
   const index = url.indexOf("media/") + "media/".length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
 };
